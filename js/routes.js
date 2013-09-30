8815785826c2f064cdd1b008094e06d718d3e2dc
gregmalcolm <greg@gregmalcolm.com>
2013-09-29 23:14:35 -0400

    Explicitly chose how the stories.show model was loaded

--- js/routes.js
+++ js/routes.js
@@ -16,3 +16,10 @@ App.StoriesIndexRoute = Ember.Route.extend({
   }
 });
 
+App.StoriesShowRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.get("store").find('story', params.story_id);
+  }
+});
+
+
