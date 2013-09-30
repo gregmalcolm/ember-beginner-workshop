9ccafc6f681ba4340ec884eb52afd766c79e7da7
gregmalcolm <greg@gregmalcolm.com>
2013-09-29 23:17:55 -0400

    Can cancel from stories.edit too

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,10 @@ App.StoriesEditController = Ember.ObjectController.extend({
   actions: {
     save: function() {
       this.get("content").save();
+    },
+    cancel: function() {
+      this.get("content").rollback();
+      this.transitionToRoute("stories.index");
     }
   },
 });
